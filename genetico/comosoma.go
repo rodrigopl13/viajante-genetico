7f92e244742924f105d7f5c7d3efee202760b4ff
Rodrigo Perez <drigo.luna@gmail.com>
2019-09-19 00:01:39 -0500

    Add calculate distance for each chromosome and inversion function

--- genetico/comosoma.go
+++ genetico/comosoma.go
@@ -2,39 +2,53 @@ package genetico
 
 import (
 	"github.com/rodrigopl13/viajante-genetico/plano"
+	"math"
 	"math/rand"
+	"sync"
+	"time"
 )
 
-type Genetic struct{
-	Population [][]int32
+type Generation struct {
+	Population  [][]int32
 	Coordenates plano.Distribution
+	Distance    []float64
 }
 
-
-func NewPopulation(population, sizeChromosome int32, distribution plano.Distribution) *Genetic {
-
+func NewGeneration(population, sizeChromosome int32, distribution plano.Distribution) *Generation {
+	var wg sync.WaitGroup
 	p := make([][]int32, population)
+	wg.Add(len(p))
 	for i := range p {
-		go randomChromosome(p, int32(i), sizeChromosome)
+		go randomChromosome(p, int32(i), sizeChromosome, &wg)
 	}
 
-	for i := range p{
-		go calculateDistance(p[i], distribution)
+	wg.Wait()
+	d := make([]float64, population)
+	for i := range p {
+		go calculateDistanceChromosome(p[i], d, i, distribution)
 	}
-
-	return &Genetic{
+	//wg.Wait()
+	return &Generation{
 		Population:  p,
 		Coordenates: distribution,
+		Distance:    d,
 	}
 }
 
-func randomChromosome(p [][]int32, chromosome, sizeChromosome int32) {
+func NextGeneration(current *Generation) {
+
+}
+
+func randomChromosome(p [][]int32, chromosome, sizeChromosome int32, wg *sync.WaitGroup) {
+	defer wg.Done()
 	c := make(map[int32]bool)
 	var random int32
-	p[chromosome] = make([]int32, sizeChromosome+1)
-	for i := 0; i<len(p[chromosome])-1; i++{
+	p[chromosome] = make([]int32, sizeChromosome)
+	for i := 0; i < len(p[chromosome]); i++ {
+		rand.Seed(time.Now().UnixNano())
 		random = rand.Int31n(sizeChromosome) + 1
 		for c[random] {
+			rand.Seed(time.Now().UnixNano())
 			random = rand.Int31n(sizeChromosome) + 1
 		}
 		c[random] = true
@@ -42,13 +56,63 @@ func randomChromosome(p [][]int32, chromosome, sizeChromosome int32) {
 	}
 }
 
-func calculateDistance(chromosome []int32, distribution plano.Distribution){
-	//var distance int32
-	for i:=0; i<len(chromosome) -1; i++ {
-		// d := mat.Sqrt(math.Pow())
+func calculateDistanceChromosome(
+	chromosome []int32,
+	distance []float64,
+	index int,
+	distribution plano.Distribution,
+) {
+	if len(chromosome) < 2 {
+		distance[index] = 0
+		return
 	}
+	var d float64
+	d = calculateDistance(
+		distribution.Cities[chromosome[0]].X,
+		distribution.Cities[chromosome[0]].Y,
+		distribution.Cities[chromosome[1]].X,
+		distribution.Cities[chromosome[1]].Y,
+	)
 
+	if len(chromosome) > 2 {
+		for i := 1; i < len(chromosome)-1; i++ {
+			d += calculateDistance(
+				distribution.Cities[chromosome[i]].X,
+				distribution.Cities[chromosome[i]].Y,
+				distribution.Cities[chromosome[i+1]].X,
+				distribution.Cities[chromosome[i+1]].Y,
+			)
+		}
+	}
+	distance[index] = d
 }
 
+func calculateDistance(x1, y1, x2, y2 int32) float64 {
+	return math.Sqrt(math.Pow(math.Abs(float64(x2-x1)), 2) + math.Pow(math.Abs(float64(y2-y1)), 2))
+}
 
+func inversion(a []int) {
+	rand.Seed(time.Now().UnixNano())
+	start := rand.Intn(len(a))
+	size := rand.Intn(len(a) - 2)
+	i := 0
+	var j, k, tmp int
+	for i <= (size / 2) {
+		j = start + i
+		if j > len(a)-1 {
+			j = j - len(a)
+		}
+		k = start + size - i
+		if k > len(a)-1 {
+			k = k - len(a)
+		}
+		tmp = a[k]
+		a[k] = a[j]
+		a[j] = tmp
+		i++
+	}
+}
 
+func intercambio() {
+
+}
--- main.go
+++ main.go
@@ -7,16 +7,24 @@ import (
 )
 
 func main() {
-	p := genetico.NewPopulation(100, 20, plano.CreateCities())
+	p := genetico.NewGeneration(100, 20, plano.CreateCities())
 	printPopulation(p.Population)
+	printDistance(p.Distance)
 }
 
-func printPopulation(p [][]int32){
+func printPopulation(p [][]int32) {
 	for i := range p {
-		fmt.Printf("%3d :: ",i+1)
-		for j := range p[i]{
+		fmt.Printf("%3d :: ", i+1)
+		for j := range p[i] {
 			fmt.Printf("%d \t", p[i][j])
 		}
 		fmt.Println()
+	}
+}
+
+func printDistance(distance []float64) {
+	for i := range distance {
+		fmt.Printf("%3d :: %.3f\n", i+1, distance[i])
+
 	}
 }
